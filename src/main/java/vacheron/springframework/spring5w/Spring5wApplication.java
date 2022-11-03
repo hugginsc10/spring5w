a6707023d0f3394bed041a545dc7d4fbe2ff8a1d
Chas Huggins <hugginsc10@gmail.com>
2022-11-02 21:46:25 -0400

    added h2-console true in application.properties

--- src/main/java/vacheron/springframework/spring5w/Spring5wApplication.java
+++ src/main/java/vacheron/springframework/spring5w/Spring5wApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Spring5wApplication {
-
+//	have to make sure h2.console is enabled in application.properties
+//	make sure you go to localhost:{port}/h2-console/ to access h2 database console
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5wApplication.class, args);
 	}
